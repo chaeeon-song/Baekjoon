f61af6c1a4daa97592cbd120793b2cd79a522423
chaeeon-song <songcindy0119@gmail.com>
2026-03-26 21:05:55 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,11 +1,11 @@
 A, B, C = map(int, input().split())
 if A == B == C :
     print(f'{10000+A*1000}')
-elif A == B :
+elif A == B : #A == B or A == C 로 묶어줬으면 한 줄 줄일 수 있었음.
     print(f'{1000+A*100}')
 elif B == C :
     print(f'{1000+B*100}')
 elif C == A :
     print(f'{1000+C*100}')
 else :
-    print(max(A, B, C)*100)
+    print(max(A, B, C)*100) #max() 함수 기억하자!!
